35aead8ba24f629bdb389198b192b57fd1ac5832
Leandro <leandrosantos020788@gmail.com>
2020-10-06 10:46:59 -0300

    Update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require("./routes")
 const app = express()
-const videos = require("./data")
+
 
 app.use(express.static('public'))
 
@@ -11,42 +12,6 @@ nunjucks.configure("views", {
   express:app,
   autoescape: false,
   noCache: true
-})
-app.get("/", function(req, res){
-
-  const about = {
-  avatar_url: "https://avatars0.githubusercontent.com/u/48134318?s=460&u=1ab8aefde787685c5913a792411c4c6f4d17aba8&v=4",
-  name: "Leandro Santos ",
-  role: "Programador Web",
-  link: [
-    {name: "Github", url:" https://github.com/LHSApp"},
-    {name: "Instagram", url:" https://github.com/LHSApp"},
-    {name: "Contato", url:" https://github.com/LHSApp"},
-  ]
-
-  }
-
-  return res.render("about", {about})
-})
-
-app.get("/Portfolio", function(req, res){
-
-  return res.render("Portfolio", {items: videos})
-})
-
-
-app.get("/video", function(req, res){
-  const id= req.query.id
-
-  const video = videos.find(function(video){
-    return video.id == id
-   
-  })
-  if (!video){
-    return res.send("Video not found!")
-  }
-
-  return res.render("video", {item: video})
 })
 
 
